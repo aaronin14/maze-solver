655fbae2aacc84b416f969837c997aac7b8a99df
Aaron Nguyen <aaron14nguyen@gmail.com>
2025-01-14 18:58:20 -0500

    draw some lines

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+    l1 = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l1, "black")
     win.wait_for_close()
 
 
