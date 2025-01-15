e11903cbb8c792f3ca85a0c17036128eacdeb73c
Aaron Nguyen <aaron14nguyen@gmail.com>
2025-01-15 17:24:48 -0500

    Break entrance and exist walls

--- cell.py
+++ cell.py
@@ -20,18 +20,34 @@ class Cell:
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
-        if self.has_left_wall:
-            line = Line(Point(x1, y1), Point(x1, y2))
-            self._win.draw_line(line)
-        if self.has_right_wall:
-            line = Line(Point(x2, y1), Point(x2, y2))
-            self._win.draw_line(line)
-        if self.has_top_wall:
-            line = Line(Point(x1, y1), Point(x2, y1))
-            self._win.draw_line(line)
-        if self.has_bottom_wall:
-            line = Line(Point(x1, y2), Point(x2, y2))
-            self._win.draw_line(line)
+
+        # Left Wall
+        left_wall = Line(Point(x1, y1), Point(x1, y2))
+        left_color = "black"
+        if self.has_left_wall is False:
+            left_color = "white"
+        self._win.draw_line(left_wall, left_color)
+
+        # Right Wall
+        right_wall = Line(Point(x2, y1), Point(x2, y2))
+        right_color = "black"
+        if self.has_right_wall is False:
+            right_color = "white"
+        self._win.draw_line(right_wall, right_color)
+
+        # Top Wall
+        top_wall = Line(Point(x1, y1), Point(x2, y1))
+        top_color = "black"
+        if self.has_top_wall is False:
+            top_color = "white"
+        self._win.draw_line(top_wall, top_color)
+
+        # Bottom Wall
+        bottom_wall = Line(Point(x1, y2), Point(x2, y2))
+        bottom_color = "black"
+        if self.has_bottom_wall is False:
+            bottom_color = "white"
+        self._win.draw_line(bottom_wall, bottom_color)
 
     def drawable(self):
         return not (self._x1 is None or self._x2 is None or self._y1 is None or self._y2 is None)
--- test.py
+++ test.py
@@ -6,15 +6,22 @@ class Test(unittest.TestCase):
     def test_maze_create_cells(self):
         num_cols = 12
         num_rows = 10
-        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        maze = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
-            len(m1._cells),
+            len(maze._cells),
             num_cols,
         )
         self.assertEqual(
-            len(m1._cells[0]),
+            len(maze._cells[0]),
             num_rows,
         )
+
+    def test_break_entrance_and_exit(self):
+        num_cols = 12
+        num_rows = 10
+        maze = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertFalse(maze._cells[0][0].has_top_wall)
+        self.assertFalse(maze._cells[-1][-1].has_bottom_wall)
 
 
 if __name__ == "__main__":
