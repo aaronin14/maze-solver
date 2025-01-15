b777b82b1b22f53cce290bbbbec90c42bec749e4
Aaron Nguyen <aaron14nguyen@gmail.com>
2025-01-14 21:02:20 -0500

    Cell

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from graphics import Window, Point, Line
+from graphics import Window
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    l1 = Line(Point(50, 50), Point(400, 400))
-    win.draw_line(l1, "black")
+
+    c = Cell(win)
+    #c.has_left_wall = False
+    #c.has_right_wall = False
+    #c.has_top_wall = False
+    #c.has_bottom_wall = False
+    c.draw(50, 50, 100, 100)
+
     win.wait_for_close()
 
 
